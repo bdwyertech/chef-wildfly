811c0fc79ccc1caee9f363d597bd1d1edb3bdfb5
Brian Dwyer <bdwyer@ieee.org>
2014-03-11 18:53:50 -0400

    System Properties  -Added ability to specify system properties.

---
+++ attributes/system_properties.rb
@@ -0,0 +1,10 @@
+# encoding: UTF-8
+# => Wildfly System Properties Configuration
+
+# => Simply add properties to this array of hashes
+
+default['wildfly']['system_properties'] = [
+#  { name: 'JdbcUrl', value: 'jdbc:mysql://12.34.56.78:3306/testdb' },
+#  { name: 'JdbcUsername', value: 'testuser' },
+#  { name: 'JdbcPassword', value: 'testpass' }
+]
