cba97a37b83b6afe62712a7ac79623b21269d856
Brian Dwyer <bdwyer@ieee.org>
2014-04-04 17:19:58 -0400

    Bump for Connector/J Update  -Connector/J version updated to 5.1.30

--- attributes/mysql.rb
+++ attributes/mysql.rb
@@ -4,8 +4,8 @@
 
 # => MySQL ConnectorJ
 default['wildfly']['mysql']['enabled'] = true
-default['wildfly']['mysql']['url'] = 'http://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.29.tar.gz'
-default['wildfly']['mysql']['checksum'] = '04ad83b655066b626daaabb9676a00f6b4bc43f0c234cbafafac1209dcf1be73'
+default['wildfly']['mysql']['url'] = 'http://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.30.tar.gz'
+default['wildfly']['mysql']['checksum'] = '1986baca293f998f9ecfe8a56e6e832825048a9c466cc5b5ed91940407f1210d'
 
 # => MySQL ConnectorJ JDBC Module Name
 default['wildfly']['mysql']['mod_name'] = 'com.mysql'
