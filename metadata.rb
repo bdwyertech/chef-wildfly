4377c8811b54fe850b8a71d39875694c8bdc3675
Brian Dwyer <bdwyer@ieee.org>
2014-04-04 15:39:11 -0400

    Updated to reflect Apache v2 license

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'wildfly'
 maintainer       'Brian Dwyer - Intelligent Digital Services'
 maintainer_email 'bdwyertech'
-license          'All rights reserved'
+license          'Apache v2.0'
 description      'Installs/Configures wildfly'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
