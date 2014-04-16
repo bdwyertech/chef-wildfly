96f634a44e27112ecb1363db2d88153ef77b98cd
Brian Dwyer <bdwyer@ieee.org>
2014-04-16 17:31:44 -0400

    Bug Fix and Java Update  Java checksum should have been SHA256.  Java version bumped to 7u55

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'bdwyertech'
 license          'Apache v2.0'
 description      'Installs/Configures wildfly'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.2'
+version          '0.1.3'
 
 supports 'centos'
 
