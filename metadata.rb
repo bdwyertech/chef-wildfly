6cf867c6729e16134a79f153d229b66ce5ac2a87
Suleyman Kutlu <skutlu@schubergphilis.com>
2014-04-22 20:08:18 +0200

    Java Cookbook version

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,9 @@ maintainer_email 'bdwyertech'
 license          'Apache v2.0'
 description      'Installs/Configures wildfly'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.5'
+version          '0.1.6'
 
 supports 'centos'
 
 depends 'yum'
-depends 'java', '~> 1.22'
+depends 'java', '= 1.13.1'
