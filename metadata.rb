9c839793c56a86fbc692c3dc3bd620f383001ddc
Suleyman Kutlu <skutlu@schubergphilis.com>
2014-04-22 21:01:05 +0200

    Supported OS

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,11 @@ maintainer_email 'bdwyertech'
 license          'Apache v2.0'
 description      'Installs/Configures wildfly'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.11'
+version          '0.1.12'
 
-supports 'centos'
+%w{ centos redhat }.each do |os|
+  supports os
+end
 
 depends 'yum'
 depends 'java', '= 1.13.1'
