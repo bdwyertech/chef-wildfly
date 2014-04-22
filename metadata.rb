2c46763317157953507294ab31f9572bd009a45c
Suleyman Kutlu <skutlu@schubergphilis.com>
2014-04-22 20:23:00 +0200

    Make it Ruby way

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'bdwyertech'
 license          'Apache v2.0'
 description      'Installs/Configures wildfly'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.9'
+version          '0.1.10'
 
 supports 'centos'
 
