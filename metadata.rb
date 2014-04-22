dd39a5eb8e94e513a743eba5155766fb18b9c05f
Suleyman Kutlu <skutlu@schubergphilis.com>
2014-04-22 20:06:24 +0200

    No need for apt on RHEL

--- metadata.rb
+++ metadata.rb
@@ -4,10 +4,9 @@ maintainer_email 'bdwyertech'
 license          'Apache v2.0'
 description      'Installs/Configures wildfly'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.4'
+version          '0.1.5'
 
 supports 'centos'
 
-depends 'apt'
 depends 'yum'
 depends 'java', '~> 1.22'
