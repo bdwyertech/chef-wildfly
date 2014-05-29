d4febad01ea36bba55039480240ce359cdafc48f
Brian Dwyer <bdwyer@ieee.org>
2014-05-29 18:10:29 -0400

    Java updated to 7u60

--- attributes/java.rb
+++ attributes/java.rb
@@ -5,5 +5,5 @@
     default['java']['install_flavor'] = 'oracle'
     default['java']['oracle']['accept_oracle_download_terms'] = true
     default['java']['jdk_version'] = '7'
-    default['java']['jdk']['7']['x86_64']['url'] = 'http://download.oracle.com/otn-pub/java/jdk/7u55-b13/jdk-7u55-linux-x64.tar.gz'
-    default['java']['jdk']['7']['x86_64']['checksum'] = '86f8c25718801672b7289544119e7909de82bb48393b78ae89656b2561675697'
+    default['java']['jdk']['7']['x86_64']['url'] = 'http://download.oracle.com/otn-pub/java/jdk/7u60-b19/jdk-7u60-linux-x64.tar.gz'
+    default['java']['jdk']['7']['x86_64']['checksum'] = 'c7232b717573b057dbe828d937ee406b7a75fbc6aba7f1de98a049cbd42c6ae8'
