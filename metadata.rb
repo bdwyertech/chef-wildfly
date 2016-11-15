dd0417cfebfe0510ee1271c956bb3b5c94c798aa
Marc Arndt <marc@marcarndt.com>
2016-11-15 08:35:49 +0200

    renaming templates so they have a version number in them. This should allow the cookbook to work for version 8 and start working for version 10. In the install.rb, the version number is added to the template

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ maintainer_email 'bdwyertech'
 license          'Apache License, Version 2.0'
 description      'Installs/Configures wildfly'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.3.2'
+version          '0.3.3'
 
 supports 'centos'
 
