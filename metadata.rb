e0c617386022e44e0d76213f6f30dbb43c6b4cad
Daniel Dyba <daniel.dyba@pnmac.com>
2017-07-11 14:08:44 -0700

    Fixes FC066

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,7 @@ license          'Apache-2.0'
 description      'Installs/Configures wildfly'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.4.2'
+chef_version     '<= 12.4'
 
 supports 'centos'
 
