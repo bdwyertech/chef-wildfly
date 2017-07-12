e5145208da2a313bfc49c864722e23fc01fb072e
Daniel Dyba <daniel.dyba@gmail.com>
2017-07-11 21:58:25 -0700

    Ignore rubocop messages

--- providers/property.rb
+++ providers/property.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-# rubocop:disable LineLength, Metrics/AbcSize
+# rubocop:disable LineLength, Metrics/AbcSize, Style/MethodLength
 
 # encoding: UTF-8
 
--- recipes/install.rb
+++ recipes/install.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-# rubocop:disable LineLength
+# rubocop:disable LineLength,SymbolArray
 
 # encoding: UTF-8
 
