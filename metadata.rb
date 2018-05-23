946230fe4c7ecb8c1e6f951907769f916d5c192b
Brian Dwyer <bdwyer@IEEE.org>
2018-05-22 22:56:01 -0400

    Bump minimum Chef version in response to Chef 12 and compat_resource EOL

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ license          'Apache-2.0'
 description      'Installs/Configures wildfly'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.1'
-chef_version     '>= 12.11'
+chef_version     '>= 12.19' if respond_to?(:chef_version)
 
 supports 'centos'
 supports 'ubuntu'
